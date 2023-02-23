8136856202b9e86131812ac55787d65da9132f72
Victor Lieuthier <vgabriel@keener.io>
2023-02-22 21:06:16 -0300

    finally index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
-  <main
-    style="
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100%;
-      width: 100%;
-    "
-  >
+  <main>
+    <!-- <div class="background"></div> -->
+    <div class="blob">
+      <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+        <path
+          fill="#e5e5e5"
+          d="M42.4,-48.2C51.6,-33.2,53.3,-16.6,48.2,-5.1C43.1,6.4,31.1,12.7,21.9,19C12.7,25.3,6.4,31.5,-6,37.5C-18.4,43.5,-36.7,49.3,-50.1,43C-63.6,36.7,-72,18.4,-71.6,0.4C-71.2,-17.5,-61.8,-35,-48.4,-50C-35,-65,-17.5,-77.5,-0.5,-77.1C16.6,-76.6,33.2,-63.2,42.4,-48.2Z"
+          transform="translate(100 100)"
+        />
+      </svg>
+    </div>
     <div
       style="
         display: flex;
@@ -27,8 +29,9 @@
           </p>
         </header>
         <v-btn
-        to="/auth/login"
-        color="#00bfa6"
+          to="/auth/login"
+          class="btn-login"
+          color="#00bfa6"
           style="
             height: 3rem;
             width: 100%;
@@ -41,7 +44,7 @@
           >Login</v-btn
         >
       </div>
-      <div style="width: 50%">
+      <div class="part-two" style="width: 50%">
         <v-img src="./deliveryman.svg"></v-img>
       </div>
     </div>
@@ -49,16 +52,27 @@
 </template>
 
 <script setup>
-
 useHead({
-  title: 'Página Inicial',
-})
+  title: "Página Inicial",
+});
 definePageMeta({
-  layout: 'external',
-})
+  layout: "external",
+});
 </script>
 
 <style lang="scss" scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  background-image: url("../public/blob-wave.svg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  overflow:  hidden !important;
+}
+
 .part-one {
   display: flex;
   flex-direction: column;
@@ -72,12 +86,65 @@ definePageMeta({
     font-size: 5rem;
     font-weight: 800;
     color: #00bfa6;
+    -webkit-text-stroke: 5px #fff;
   }
 
   p {
     font-size: 1.5rem;
     font-weight: 500;
     color: #00bfa6;
+  }
+}
+
+.blob {
+  position: absolute;
+  top: 0;
+  left: 0;
+  fill: #023f92;
+  width: 40vmax;
+  z-index: -1;
+  animation: move 10s ease-in-out infinite;
+  transform-origin: 50% 50%;
+  overflow:  hidden !important;
+}
+
+@keyframes move {
+  0% {
+    transform: scale(1) translate(10px, -30px);
+  }
+  38% {
+    transform: scale(0.8, 1) translate(80vw, 30vh) rotate(160deg);
+  }
+  40% {
+    transform: scale(0.8, 1) translate(80vw, 30vh) rotate(160deg);
+  }
+  78% {
+    transform: scale(1.3) translate(0vw, 30vh) rotate(-20deg);
+  }
+  80% {
+    transform: scale(1.3) translate(0vw, 30vh) rotate(-20deg);
+  }
+  100% {
+    transform: scale(1) translate(10px, -30px);
+  }
+}
+
+@media (max-width: 768px) {
+  .btn-login {
+    padding:0 8rem;
+  }
+  .part-one {
+
+    height: 100vh;
+
+
+    h1 {
+      -webkit-text-stroke: 0 !important;
+    }
+  }
+
+  .part-two {
+    display: none;
   }
 }
 </style>
